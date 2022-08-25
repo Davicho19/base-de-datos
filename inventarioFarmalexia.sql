209d2ee6f6b37466bcf9045ac53194bf4a980d7b
Davicho19 <davico1933@hotmail.com>
2022-08-25 12:19:09 -0400

    Version8

--- inventarioFarmalexia.sql
+++ inventarioFarmalexia.sql
@@ -4,13 +4,16 @@
 
 --hola david ggggg
 ---segundo commit
+
+
+---nuevas modificaciones de la version8
 DROP DATABASE IF EXISTS farmalexia;
 CREATE DATABASE IF NOT EXISTS farmalexia;
 USE farmalexia;
 
 
 CREATE TABLE persona(
-idpersona INT AUTO_INCREMENT PRIMARY KEY,
+idpersona INT PRIMARY KEY,
 ci INT NOT NULL UNIQUE,
 primerNombre VARCHAR(25) NOT NULL,
 seguntoNombre VARCHAR(25) NULL,
@@ -20,15 +23,24 @@ sexo ENUM ('M','F'),
 fechaNacimiento DATE
 )ENGINE=InnoDB;
 
-
 CREATE TABLE rol(
-idrol INT AUTO_INCREMENT PRIMARY KEY,
+idrol INT PRIMARY KEY,
 tipoPersona VARCHAR(25) UNIQUE NOT NULL
 )ENGINE=InnoDB;
 
 
+CREATE TABLE identidad(
+idIdentidad INT AUTO_INCREMENT PRIMARY KEY,
+contrasenha VARCHAR(25) UNIQUE NOT NULL,
+activo bool NOT NULL,
+idpersona INT NOT NULL,
+idrol INT NOT NULL,
+FOREIGN KEY(idpersona) REFERENCES persona(idpersona) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY(idrol) REFERENCES rol(idrol) ON UPDATE CASCADE ON DELETE CASCADE
+)ENGINE=InnoDB;
+
 CREATE TABLE cliente(
-idcliente INT AUTO_INCREMENT PRIMARY KEY,
+idcliente INT PRIMARY KEY NULL,
 primerNombre VARCHAR(25) NOT NULL,
 seguntoNombre VARCHAR(25) NULL,
 apellidoPaterno VARCHAR(25) NOT NULL,
@@ -36,9 +48,19 @@ apellidoMaterno VARCHAR(25) NULL
 )ENGINE=InnoDB;
 
 
+
 CREATE TABLE rolCliente(
-idRolCliente INT AUTO_INCREMENT PRIMARY KEY,
-tipoCliente VARCHAR(25) NOT NULL
+idRolCliente INT PRIMARY KEY NULL,
+tipoCliente VARCHAR(25) NOT NULL    
+)ENGINE=InnoDB;
+
+
+CREATE TABLE registroCliente(
+idRegistroCliente INT AUTO_INCREMENT PRIMARY KEY,
+idcliente INT NULL,
+idRolCliente INT NULL, 
+FOREIGN KEY(idcliente) REFERENCES cliente(idcliente) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY(idRolCliente) REFERENCES rolCliente(idcliente) ON UPDATE CASCADE ON DELETE CASCADE
 )ENGINE=InnoDB;
 
 
@@ -57,19 +79,6 @@ nombreProveedor VARCHAR(25)
 )ENGINE=InnoDB;
 
 
-
-CREATE TABLE identidad(
-idIdentidad INT AUTO_INCREMENT PRIMARY KEY,
-contrasenha VARCHAR(25) UNIQUE NOT NULL,
-activo bool NOT NULL,
-idpersona INT NOT NULL,
-idrol INT NOT NULL,
-FOREIGN KEY(idpersona) REFERENCES persona(idpersona) ON UPDATE CASCADE ON DELETE CASCADE,
-FOREIGN KEY(idrol) REFERENCES rol(idrol) ON UPDATE CASCADE ON DELETE CASCADE
-)ENGINE=InnoDB;
-
-
-
 --tablas relacionales
 CREATE TABLE registroFarmaco(
 idRegistro INT AUTO_INCREMENT PRIMARY KEY,
@@ -83,13 +92,15 @@ FOREIGN KEY(idprovedor) REFERENCES proveedores(idprovedor) ON UPDATE CASCADE ON 
 )ENGINE=InnoDB;
 
 CREATE TABLE salidaMedicamentos(
-idSalidaMedicamentos INT AUTO_INCREMENT PRIMARY KEY NOT null,
+idSalidaMedicamentos INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 fechaHoraVenta DATETIME NOT null,
 cantidaSalidaMendicamentos INT NOT NULL,
 idRegistro INT NOT NULL,
 ididentidad INT NOT NULL,
+idRegistroCliente INT NULL,
 FOREIGN KEY(idRegistro) REFERENCES registroFarmaco(idRegistro) ON UPDATE CASCADE ON DELETE CASCADE,
 FOREIGN KEY(ididentidad) REFERENCES identidad(ididentidad) ON UPDATE CASCADE ON DELETE CASCADE
+FOREIGN KEY(idRegistroCliente) REFERENCES registroCliente(idRegistroCliente) ON UPDATE CASCADE ON DELETE CASCADE
 )ENGINE=InnoDB;
 
 
